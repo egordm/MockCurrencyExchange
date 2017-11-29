385ab2e89adb36ab6ff640c62a7b26053f8a6ddc
Egor Dmitriev <egordmitriev2@gmail.com>
2017-11-29 21:39:56 +0100

    Added reactjs

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.react('resources/assets/js/app.jsx', 'public/js')
+	.sass('resources/assets/sass/app.scss', 'public/css')
+	.sourceMaps();
