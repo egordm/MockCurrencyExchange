680e183fdd26a6078d76f72546dbf08aec00af5f
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-05 18:15:17 +0100

    Moved generating mock data to utils

--- resources/assets/js/exchange/reducers/reducer-open-orders.js
+++ resources/assets/js/exchange/reducers/reducer-open-orders.js
@@ -1,20 +1,9 @@
-import {format} from "d3-format";
+import {getOpenOrders} from "../utils/MockData";
 
-const generateOrders = () => {
-	let ret = [];
-	for(let i = 0; i < 60; ++i) {
-		ret.push({
-			total: format("(.8f")(Math.random() * 1000),
-			amount: format("(.4f")(Math.random() * 1000),
-			price: format("(.2f")(Math.random() * (20000 - 14000) + 14000),
-		});
-	}
-	return ret;
-};
 
 export default function () {
 	return {
-		bids: generateOrders(),
-		asks: generateOrders()
+		bids: getOpenOrders(),
+		asks: getOpenOrders()
 	};
 }
--- resources/assets/js/exchange/reducers/reducer-order-history.js
+++ resources/assets/js/exchange/reducers/reducer-order-history.js
@@ -1,14 +1,5 @@
-import {format} from "d3-format";
+import {getOrderHistory} from "../utils/MockData";
 
 export default function () {
-	let ret = [];
-	for(let i = 0; i < 60; ++i) {
-		ret.push({
-			time: '18:32:12',
-			amount: format("(.3f")(Math.random() * 1000),
-			price: format("(.2f")(Math.random() * (20000 - 14000) + 14000),
-			type: Math.random() >= 0.5 ? 'buy' : 'sell'
-		});
-	}
-	return ret;
+	return getOrderHistory();
 }
