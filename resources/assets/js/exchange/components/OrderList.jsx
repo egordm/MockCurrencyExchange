dcd42102022e5c1c740c88e85550a2a5d7827f90
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-05 12:02:27 +0100

    Added orderlist component

---
+++ resources/assets/js/exchange/components/OrderList.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
+
+export default class OrderList extends Component {
+	static propTypes = {
+		tableClass: PropTypes.string,
+		data: PropTypes.array.isRequired,
+		columns: PropTypes.array.isRequired,
+		typeField: PropTypes.string // TODO: get type recognizer function instead
+	};
+
+	renderOrder = (order, i) => {
+		let columnData = [];
+		let type = this.props.typeField ? order[this.props.typeField] : '';
+		for(let column of this.props.columns) columnData.push(<td className={column} key={column}>{order[column]}</td>)
+		return <tr key={i} className={type}>{columnData}</tr>
+	};
+
+	render() {
+		return <div className="table-wrapper">
+			<table className={'table order-table ' + this.props.tableClass}>
+				<tbody>
+				{this.props.data.map(this.renderOrder)}
+				</tbody>
+			</table>
+		</div>;
+	}
+}
