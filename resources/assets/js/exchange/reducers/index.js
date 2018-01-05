e1e14a0a433281234166dccd89bc3f287971463e
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-05 18:20:38 +0100

    Added all reducers

--- resources/assets/js/exchange/reducers/index.js
+++ resources/assets/js/exchange/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import OrderHistoryReducer from './reducer-order-history';
 import OpenOrdersReducer from './reducer-open-orders';
+import ChartReducer from './reducer-chart';
 
 const allReducers = combineReducers({
 	order_history: OrderHistoryReducer,
-	open_orders: OpenOrdersReducer
+	open_orders: OpenOrdersReducer,
+	chart: ChartReducer,
 });
 
 export default allReducers;
