0668163ee4f452eeea387f16d15d0ddb6e0803fc
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-10 23:13:32 +0100

    Styled chart settings

--- resources/assets/js/exchange/constants/ChartStyles.js
+++ resources/assets/js/exchange/constants/ChartStyles.js
@@ -1,5 +1,7 @@
 import {AREA, CANDLESTICK, HEIKIN_ASHI, KAGI, LINE, POINT_FIGURE, RENKO} from "./ChartTypes";
 
+export const chartSettingHeight = 26;
+
 export const chartMargin = {left: 30, right: 50, top: 0, bottom: 30};
 
 export const secondaryChartHeight = 100;
