7acb69701c08afe97f5a66e5a47d1cb627884acd
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-12 23:27:28 +0100

    Updated data processing to merge candle data

--- resources/assets/js/exchange/utils/DataProcessing.js
+++ resources/assets/js/exchange/utils/DataProcessing.js
@@ -5,3 +5,9 @@ export function processCandles(data) {
 		return el;
 	})
 }
+
+export function mergeCandles(oldData, newData) {
+	if(!oldData) return newData;
+	while(oldData[oldData.length - 1].close_time > newData[0].open_time) oldData.pop();
+	return oldData.concat(newData);
+}
