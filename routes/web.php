c74103687853fe7580274252ef3ebb2efa2ddbc8
luukvnes <34107188+luukvnes@users.noreply.github.com>
2018-01-14 21:12:13 +0100

    3

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,17 @@
 */
 //Auth::routes();
 
-
 Route::post('/login', 'AuthController@login')->name('login');
 Route::post('/register', 'AuthController@register')->name('register');
 Route::post('/logout', 'AuthController@logout')->name('logout');
 
 Route::get('/', 'MainController@index')->name('home');
 Route::get('/exchange', 'MainController@exchange')->name('exchange');
-Route::get('/create', 'AuthController@create')->name('create');
+Route::get('/register', 'AuthController@create')->name('register');
+Route::get('/logout', 'AuthController@destroy')->name('logout');
+Route::get('/login', 'AuthController@login')->name('login');
+
 
 Route::get('/test', function () {
 	return Response::download(public_path('downloads/BTCUSDT_15m_1513316700_1514397599.csv'));
 });
-
