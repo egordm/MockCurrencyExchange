cbfe93294dbad855d4aeb08888d41e26d426ac8f
luukvnes <34107188+luukvnes@users.noreply.github.com>
2018-01-16 14:28:11 +0100

    naam en ww wijzigen

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Hash;
 
 class AccountController extends Controller
 {
@@ -12,20 +14,39 @@ class AccountController extends Controller
         return view('/account');
     }
 
-    public function postChangeName(request $request)
+    public function postChange(request $request)
     {
+        if (Input::get('name')) {
 
-        if(strcmp(Auth::user()->name, $request->get('new-name')) == 0){
-            //Current password and new password are same
-            return redirect()->back()->with("error","New Name cannot be same as your current password. Please choose a different name.");
+            if (strcmp(Auth::user()->name, $request->get('new-name')) == 0) {
+                //Current name and new name are same
+                return redirect()->back()->with("error", "New Name cannot be same as your current password. Please choose a different name.");
+            }
+
+            //Change Name
+            $user = Auth::user();
+            $user->Name = $request->get('new-name');
+            $user->save();
+
+            return redirect()->back()->with("success", "Name changed successfully !");
         }
+        else {
+            if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+                // The passwords matches
+                return redirect()->back()->with("error", "Your current password does not matches with the password you provided. Please try again.");
+            }
 
-        //Change Password
-        $user = Auth::user();
-        $user->Name = $request->get('new-name');
-        $user->save();
+            if (strcmp($request->get('current-password'), $request->get('new-password')) == 0) {
+                //Current password and new password are same
+                return redirect()->back()->with("error", "New Password cannot be same as your current password. Please choose a different password.");
+            }
 
-        return redirect()->back()->with("success","Password changed successfully !");
-
+            //Change Password
+            $user = Auth::user();
+            $user->password = bcrypt($request->get('new-password'));
+            $user->save();
+            return redirect()->back()->with("success", "Name changed successfully !");
+        }
     }
+
 }
