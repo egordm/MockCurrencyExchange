28ef5f820d32c7fa58d8a206ef7bb2ac86674df9
luukvnes <34107188+luukvnes@users.noreply.github.com>
2018-01-16 14:38:11 +0100

    5

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -33,19 +33,19 @@ class AccountController extends Controller
         else {
             if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
                 // The passwords matches
-                return redirect()->back()->with("error", "Your current password does not matches with the password you provided. Please try again.");
+                return ['matches' => true];
             }
 
             if (strcmp($request->get('current-password'), $request->get('new-password')) == 0) {
                 //Current password and new password are same
-                return redirect()->back()->with("error", "New Password cannot be same as your current password. Please choose a different password.");
+                return ['matches2' => true];
             }
 
             //Change Password
             $user = Auth::user();
             $user->password = bcrypt($request->get('new-password'));
             $user->save();
-            return redirect()->back()->with("success", "Name changed successfully !");
+            return ['success' => true];
         }
     }
 
