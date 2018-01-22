203ba89b4a4c35fb518a5b33099d9186649e48c9
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-22 20:51:52 +0100

    Added form view

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,99 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{config('branding.title')}} - @yield('title')</title>
-    <meta name="description" content="@yield('meta_description')">
-    <meta name="keywords" content="@yield('meta_keywords')">
-
-    <!-- Style -->
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
-    <!-- Javascript -->
-    <script src="{{ asset('/js/manifest.js') }}"></script>
-    <script src="{{ asset('/js/vendor.js') }}"></script>
-
-    <script>
-        window.Laravel = <?php echo json_encode(['csrfToken' => csrf_token()]); ?>;
-    </script>
-</head>
+@include('partials.head')
 <body>
 
-
-<nav class="navbar navbar-expand-md fixed-top @yield('navbar_class')" color-on-scroll="500">
-    <div class="container">
-        <div class="navbar-translate">
-            <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse"
-                    data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span class="navbar-toggler-bar"></span>
-                <span class="navbar-toggler-bar"></span>
-                <span class="navbar-toggler-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{route('home')}}">{{config('branding.title')}}</a>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarToggler">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="{{route('home')}}" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#about" class="nav-link">About</a>
-                </li>
-                @if (Auth::check())
-                    <li class="nav-item">
-                        <a href="/account{{--{{route('account')}}--}}" class="nav-link">Account</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/logout" class="nav-link">Logout</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/portfolio" class="nav-link">Portfolio</a>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a href="/login" class="nav-link">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="/register" class="nav-link">Register</a>
-                    </li>
-                @endif
-                <li class="nav-item">
-                    <a href="{{route('exchange')}}" class="btn btn-primary btn-round">Exchange</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
+@include('partials.navbar')
 @yield('header')
 
 <div class="main root">
-    @yield('content')
+	@yield('content')
 </div>
 
-<footer class="footer section-dark">
-    <div class="container">
-        <div class="row">
-            <nav class="footer-nav">
-                <ul>
-                    <li><a href="{{route('home')}}">Home</a></li>
-                    <li><a href="{{route('exchange')}}">Exchange</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
-            </nav>
-            <div class="credits ml-auto">
-					<span class="copyright">
-						© <script>document.write(new Date().getFullYear())</script>, C-MEX
-					</span>
-            </div>
-        </div>
-    </div>
-</footer>
+@include('partials.footer')
 
 <script src="{{ asset('/js/app.js') }}"></script>
 </body>
