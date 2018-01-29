3bb452c1526898223ed59b6d116f7e3d323f7a93
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-29 01:21:18 +0100

    Registered orderbook synchronize job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,41 +2,46 @@
 
 namespace App\Console;
 
+use App\Jobs\MergeExternalDepthJob;
+use App\Models\ValutaPair;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		//
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		foreach (ValutaPair::all() as $pair) {
+			//$schedule->job(new MergeExternalDepthJob($pair))->everyMinute();
+			$schedule->job(new MergeExternalDepthJob($pair))->everyThirtyMinutes();
+		}
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+	}
 
-        require base_path('routes/console.php');
-    }
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+		$this->load(__DIR__ . '/Commands');
+
+		require base_path('routes/console.php');
+	}
 }
