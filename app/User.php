b7cbe9fe3c38246b1cf5970e07eba3618c1144ad
Egor Dmitriev <egordmitriev2@gmail.com>
2018-01-29 01:18:26 +0100

    Added placeholder for portfolio

--- app/User.php
+++ app/User.php
@@ -35,10 +35,14 @@ use Laravel\Passport\HasApiTokens;
  * @mixin \Eloquent
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Balance[] $balances
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $open_orders
  */
 class User extends Authenticatable
 {
 	use HasApiTokens, Notifiable;
+
+	const ROLE_PLEB = 0;
+	const ROLE_BOT = 13;
 
 	/**
 	 * The attributes that are mass assignable.
@@ -63,23 +67,46 @@ class User extends Authenticatable
 		$this->attributes['password'] = bcrypt($password);
 	}
 
-	public function balances() {
+	public function balances()
+	{
 		return $this->hasMany(Balance::class);
 	}
 
-    public function orders()
-    {
-        return $this->hasMany(Order::class);
+	public function orders()
+	{
+		return $this->hasMany(Order::class);
 	}
 
-    public function getBalance(Valuta $valuta)
-    {
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $balance = \App::get(BalanceRepository::class)->skipPresenter()->getBalance($this, $valuta)->first();
-        return (!$balance) ? 0 : $balance->quantity;
+	public function open_orders()
+	{
+		return $this->hasMany(Order::class)->where('orders.status', '=', Order::STATUS_OPEN);
 	}
 
-	public function getHaltedBalance(Valuta $valuta) {
+	public function getBalance(Valuta $valuta)
+	{
+		if ($this->isBot()) return PHP_FLOAT_MAX;
+
+		/** @noinspection PhpUnhandledExceptionInspection */
+		$balance = \App::get(BalanceRepository::class)->skipPresenter()->getBalance($this, $valuta)->first();
+		return (!$balance) ? 0 : $balance->quantity;
+	}
+
+	public function getHaltedBalance(Valuta $valuta)
+	{
+		if ($this->isBot()) return 0;
+		/** @noinspection PhpUnhandledExceptionInspection */
 		return \App::get(BalanceRepository::class)->getHaltedBalance($this, $valuta);
+	}
+
+	public function isBot()
+	{
+		return $this->role == self::ROLE_BOT;
+	}
+
+	public static function bots($with = null)
+	{
+		$ret = User::where('role', '=', User::ROLE_BOT);
+		if(!empty($with)) $ret = $ret->with($with);
+		return $ret->get();
 	}
 }
