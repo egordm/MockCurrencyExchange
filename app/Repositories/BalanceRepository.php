88967995a290a61cf1177403b47d09174ef8f471
luukvnes <34107188+luukvnes@users.noreply.github.com>
2018-01-30 13:13:37 +0100

    Werkt nu miss ook bij BTC/iets

--- app/Repositories/BalanceRepository.php
+++ app/Repositories/BalanceRepository.php
@@ -24,54 +24,63 @@ use DB;
 
 class BalanceRepository extends PresentableRepository
 {
-	/**
-	 * Specify Model class name
-	 *
-	 * @return mixed
-	 */
-	public function model()
-	{
-		return Balance::class;
-	}
+    /**
+     * Specify Model class name
+     *
+     * @return mixed
+     */
+    public function model()
+    {
+        return Balance::class;
+    }
 
-	/**
-	 * @param User $user
-	 * @param null $symbols
-	 * @return mixed
-	 * @throws \Illuminate\Support\Facades\ContainerExceptionInterface
-	 * @throws \Illuminate\Support\Facades\NotFoundExceptionInterface
-	 * @throws \Prettus\Repository\Exceptions\RepositoryException
-	 */
-	public function getBalances(User $user, $symbols = null)
-	{
-		$useValuta = !empty($symbols);
-		$repo = $useValuta ? \App::get(ValutaRepository::class) : $this;
-		$repo->pushCriteria(new HoldQuantityCriteria(\Auth::user(), $useValuta ? 'valuta.id' : 'balances.valuta_id'));
-		if (!$useValuta) return $this->with(['valuta'])->findWhere(['balances.user_id' => $user->id]);
+    /**
+     * @param User $user
+     * @param null $symbols
+     * @return mixed
+     * @throws \Illuminate\Support\Facades\ContainerExceptionInterface
+     * @throws \Illuminate\Support\Facades\NotFoundExceptionInterface
+     * @throws \Prettus\Repository\Exceptions\RepositoryException
+     */
+    public function getBalances(User $user, $symbols = null)
+    {
+        $useValuta = !empty($symbols);
+        $repo = $useValuta ? \App::get(ValutaRepository::class) : $this;
+        $repo->pushCriteria(new HoldQuantityCriteria(\Auth::user(), $useValuta ? 'valuta.id' : 'balances.valuta_id'));
+        if (!$useValuta) return $this->with(['valuta'])->findWhere(['balances.user_id' => $user->id]);
 
-		$repo->pushCriteria(new BySymbolCriteria($symbols, !$useValuta));
-		$repo->pushCriteria(new WithBalanceCriteria(\Auth::user()));
-		$repo->setPresenter(BalanceValutaPresenter::class);
-		return $repo->all();
-	}
+        $repo->pushCriteria(new BySymbolCriteria($symbols, !$useValuta));
+        $repo->pushCriteria(new WithBalanceCriteria(\Auth::user()));
+        $repo->setPresenter(BalanceValutaPresenter::class);
+        return $repo->all();
+    }
 
-	public function getBalance(User $user, Valuta $valuta)
-	{
-		return $this->findWhere(['user_id' => $user->id, 'valuta_id' => $valuta->id]);
-	}
-
+    public function getBalance(User $user, Valuta $valuta)
+    {
+        return $this->findWhere(['user_id' => $user->id, 'valuta_id' => $valuta->id]);
+    }
 
 
     public function getConvertedBalance(Collection $balances)
     {
         $convertedbalance = 0;
         $USDollarid = DB::table('valuta')->where('name', 'US Dollar')->first()->id;
+        $Bitcoinid = DB::table('valuta')->where('name', 'Bitcoin')->first()->id;
+        $valutapairUSDBTCid = DB::table('valuta_pairs')->where('valuta_primary_id', $USDollarid)->where('valuta_secondary_id', $Bitcoinid)->first()->id;
+
         foreach ($balances as $balance):
             if (!($balance->valuta->name == 'US Dollar')) {
-                $valutapair = DB::table('valuta_pairs')->where('valuta_primary_id', $USDollarid)->where('valuta_secondary_id', $balance->valuta->id)->first()->id;
-                $order = DB::table('candlestick_nodes')->where('valuta_pair_id', $valutapair)->Orderby('close_time', 'desc')->first();
-                $conversion = $conversion = ($order->high + $order->low) / 2;
-                $convertedbalance = $convertedbalance + $conversion * $balance->quantity;
+                $valutapair = DB::table('valuta_pairs')->where('valuta_secondary_id', $balance->valuta->id)->first();
+                $order = DB::table('candlestick_nodes')->where('valuta_pair_id', $valutapair->id)->Orderby('close_time', 'desc')->first();
+                if ($valutapair->valuta_primary->id == $USDollarid) {
+                    $conversion = ($order->high + $order->low) / 2;
+                    $convertedbalance = $convertedbalance + $conversion * $balance->quantity;
+                } else {
+                    $order2 = DB::table('candlestick_nodes')->where('valuta_pair_id', $valutapairUSDBTCid)->Orderby('close_time', 'desc')->first();
+                    $conversion = ($order->high + $order->low) / 2;
+                    $conversion2 = ($order2->high + $order2->low) / 2;
+                    $convertedbalance = $convertedbalance + $conversion2 * $conversion * $balance->quantity;
+                }
             } else {
                 $convertedbalance = $convertedbalance + $balance->quantity;
             }
@@ -80,25 +89,25 @@ class BalanceRepository extends PresentableRepository
         return $convertedbalance;
     }
 
-	/**
-	 * Get balance for given valuta that is not useable. Not usable means it has been reserved for an order.
-	 * @param User $user
-	 * @param Valuta $valuta
-	 * @return int
-	 * @throws \Illuminate\Support\Facades\ContainerExceptionInterface
-	 * @throws \Illuminate\Support\Facades\NotFoundExceptionInterface
-	 */
-	public function getHaltedBalance(User $user, Valuta $valuta)
-	{
-		/** @noinspection PhpUnhandledExceptionInspection */
-		$orderRepository = \App::get(OrderRepository::class);
-		$orderRepository->pushCriteria(new OrderValutaCriteria($valuta));
-		$orderRepository->pushCriteria(new ActiveOrderCriteria());
-		$orders = $orderRepository->skipPresenter()->getOrders($user);
-		$orderRepository->clearCriteria();
+    /**
+     * Get balance for given valuta that is not useable. Not usable means it has been reserved for an order.
+     * @param User $user
+     * @param Valuta $valuta
+     * @return int
+     * @throws \Illuminate\Support\Facades\ContainerExceptionInterface
+     * @throws \Illuminate\Support\Facades\NotFoundExceptionInterface
+     */
+    public function getHaltedBalance(User $user, Valuta $valuta)
+    {
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $orderRepository = \App::get(OrderRepository::class);
+        $orderRepository->pushCriteria(new OrderValutaCriteria($valuta));
+        $orderRepository->pushCriteria(new ActiveOrderCriteria());
+        $orders = $orderRepository->skipPresenter()->getOrders($user);
+        $orderRepository->clearCriteria();
 
-		$ret = 0;
-		foreach ($orders as $order) $ret += $order->sellQuantity();
-		return $ret;
-	}
+        $ret = 0;
+        foreach ($orders as $order) $ret += $order->sellQuantity();
+        return $ret;
+    }
 }
